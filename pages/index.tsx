2ea53e258572838a9772dd5aa9c2fb4a79e292b6
PEGASUS Dev Team <dev@pegasus.com>
2025-12-12 14:53:31 -0300

    style(home): aplicar fundo azul/gradiente na pagina inicial

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,14 @@ import { useTyping } from "@/hooks/useTyping";
 export default function Home() {
   useTyping();
   return (
-    <>
+    <div className="relative">
+      {/* Fundo azul/gradiente apenas na Home */}
+      <div className="home-blue-bg pointer-events-none fixed inset-0 -z-10" />
       <Hero />
       <OurStorySection />
       <HistorySection />
       <JobsSection />
       <ConclusionSection />
-    </>
+    </div>
   );
 }
